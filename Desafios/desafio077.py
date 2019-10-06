a83f5909ca722dc2a87320471ee64fbac46f89cf
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-10-06 12:27:18 -0300

    Exercicio 77

--- Desafios/desafio077.py
+++ Desafios/desafio077.py
@@ -1,3 +1,15 @@
 palavras = ('APRENDER', 'LER', 'ESTUDAR', 'PYTHON', 'RICARDO',
             'FUTURO', 'CALENDARIO', 'OUTUBRO', 'TRABALHAR',
             'SETEMBRO', 'TRINTA', 'BATATA', 'RODOLFO')
+
+print('-='*20)
+print(' '*10, 'CONTADOR DE VOGAIS')
+print('-='*20)
+
+for i in palavras:
+    print(f'\nNa palavra {i} temos', end=' ')
+    for letra in i:
+        if letra in 'ÁÃÂAÉÊEIÍÓÕÔOÚÛU':
+            print(f'{letra}', end=' ')
+print('\n')
+print('-='*20)
