13e9662896993214ad705fbfae099040c10e1229
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2019-10-18 18:23:09 -0300

    exercicios 88

--- Desafios/desafio088.py
+++ Desafios/desafio088.py
@@ -1,13 +1,31 @@
 import random
 import time
 
+lista = list()
+jogos = list()
+total = 1
+
 print('-' * 30)
 print(' ' * 5, 'JOGA NA MEGA SENA')
 print('-' * 30)
 
-jogos = int(input('Quantos jogos voce quer que eu sorteie? '))
-print('-=' * 3, f' SORTEANDO {jogos} JOGOS ', '-=' * 3)
+quantidade = int(input('Quantos jogos voce quer que eu sorteie? '))
+print('-=' * 3, f' SORTEANDO {quantidade} JOGOS ', '-=' * 3)
 
-for i in range(0, jogos):
-    numeros =
-    print(f'Jogo {i+1}: {numeros}')
+while total <= quantidade:
+    cont = 0
+    while True:
+        numeros = random.randint(1, 60)
+        if numeros not in lista:
+            lista.append(numeros)
+            cont += 1
+        if cont >= 6:   # Se ja sorteou 6 numeros
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    time.sleep(1)
+print('-=' * 5, ' BOA SORTE ', '-=' * 5)
